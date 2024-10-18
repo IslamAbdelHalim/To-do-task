93e5887253a70b7bfcd644931eba8d5d7d4e848c
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-10-19 01:06:26 +0300

    adding user component

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input() name: string = '';
+  @Input() image: string = '';
+}
